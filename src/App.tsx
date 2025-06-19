5ae23427e60cb9462a75050045939c47302dd6a3
ChristianTertius <ct160103@gmail.com>
2025-06-20 05:25:50 +0700

    style(ui): change home title to white

--- src/App.tsx
+++ src/App.tsx
@@ -89,7 +89,7 @@ function App() {
           <div className="bg-black/10 w-full h-full fixed"></div>
           <DynamicHeader />
           <div className="flex sm:gap-10 flex-wrap justify-center shadow-sm py-2 px-4 rounded-full backdrop-blur-sm text-shadow-sm text-shadow-white/10">
-            <h1 className="font-display text-2xl font-semibold">
+            <h1 className="font-display text-2xl font-extralight text-white">
               This is Home Screen
             </h1>
           </div>
